08f389d1af411e0a6742529407394acaa3fe378f
Hossam Fathi <50254143+HossamFathi@users.noreply.github.com>
2021-04-22 23:53:35 +0200

    Repository  Interface  and Logic

---
+++ Repository/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OP.Infrastructure.Repository.Interfaces
+{
+    public interface IRepository<T> : IDisposable  where T : class 
+    {
+        bool SaveChange();
+        T Add(T Entity);
+        bool AddRange(ICollection<T> Entity);
+
+        T AddWithoutSave(T Enitity);
+        bool Update(T Entity);
+        bool UpdateWithoutSave(T Entity);
+     
+        T Get(int id);
+         T  Get(string id);
+        IQueryable<T> GetALl();
+
+    }
+}
---
+++ Repository/Logic/GeneralRepository.cs
@@ -0,0 +1,122 @@
+﻿using Model;
+using OP.Infrastructure.Repository.Interfaces;
+using OP.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OP.Infrastructure.Repository.Logic
+{
+
+    public class GeneralRepository<T> :   IRepository<T> where T : class  
+    {
+        private readonly OPedia db;
+        private  readonly DbSet<T> table;
+        public GeneralRepository()
+        {
+            db = new OPedia();
+           
+            table = db.Set<T>();
+
+        }
+
+        
+        public T Add(T Entity)
+        {
+            try
+            {
+                AddWithoutSave(Entity);
+                if (SaveChange())
+                    return Entity;
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        public IQueryable<T> GetALl() {
+            return table;
+        
+        }
+        public T AddWithoutSave(T Enitity)
+        {
+            table.Add(Enitity);
+            return Enitity;
+        }
+
+        public T Get(int id)
+        {
+           
+            return table.Find(id);
+        }
+
+        public bool SaveChange()
+        {
+            try
+            {
+                return db.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Update(T Entity)
+        {
+            if (UpdateWithoutSave(Entity)) 
+            {
+                return SaveChange();
+            }
+            return false;
+        }
+
+        public bool UpdateWithoutSave(T Entity)
+        {
+            try
+            {
+                table.Attach(Entity);
+                db.Entry(Entity).State = EntityState.Modified;
+                return true;
+
+            }
+            catch(Exception ex) {
+               
+
+                    return false;
+                
+
+               
+            
+            }
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public bool AddRange(ICollection<T> Entity)
+        {
+            try
+            {
+                table.AddRange(Entity);
+                return SaveChange();
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public virtual T Get(string id)
+        {
+          return  table.Find(id);
+        }
+    }
+}
